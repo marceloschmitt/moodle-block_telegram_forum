4322a4aacedf986d8005bdd2945f5ad2360d10ce
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2021-01-31 09:57:29 -0300

    Update version.php

--- version.php
+++ version.php
@@ -25,5 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_telegram_forum';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494).
-$plugin->version = 2021013101;  // YYYYMMDDHH (year, month, day, 24-hr time).
+$plugin->version = 2021013102;  // YYYYMMDDHH (year, month, day, 24-hr time).
 $plugin->supported = [39, 40];
+$plugin->requires = 2019111800;
+$plugin->maturity = MATURITY_RC;
+$plugin->release = 'v1.0';
