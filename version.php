2e04b4a270832dd7e6d9fc6c54e731b033cfa4d1
Marcelo Augusto Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2021-03-17 16:37:38 -0300

    Update version.php
    Change release.
    This version will permit to choose foruns inside the course.

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_telegram_forum';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494).
-$plugin->version = 2021031501;  // YYYYMMDDHH (year, month, day, 24-hr time).
+$plugin->version = 2021031701;  // YYYYMMDDHH (year, month, day, 24-hr time).
 $plugin->supported = [39, 40];
 $plugin->requires = 2019111800;
 $plugin->maturity = MATURITY_RC;
-$plugin->release = 'v1.0';
+$plugin->release = 'v2.0';
