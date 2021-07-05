6f3f5b54a028ae160f0fdf4cdeff6599b9ca48f7
Marcelo Augusto Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2021-07-05 14:18:08 -0300

    Issue #5
    Inccorrect use of curl_init.

--- classes/observer.php
+++ classes/observer.php
@@ -84,14 +84,9 @@ class block_telegram_forum_observer {
             'chat_id' => $channelid,
             'text' => $text,
         ];
-        $ch = curl_init($website . '/sendMessage');
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $result = curl_exec($ch);
-        curl_close($ch);
+        $curl = new curl();
+        $url = $website . '/sendMessage';
+        $result = $curl->get($url, $params);
         return true;
     }
 
