4735149fecc1c16b40a94b21c1e482ac035a2bf5
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2023-01-16 10:02:55 -0300

    Update observer.php

--- classes/observer.php
+++ classes/observer.php
@@ -22,8 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  **/
 
-defined('MOODLE_INTERNAL') || die();
-
 class block_telegram_forum_observer {
     /**
      * Event processor - user created
